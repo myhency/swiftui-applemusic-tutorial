d8fbe54228f202abaf742b4b4e544e48ed7daf95
myhency <hency.yeo@gmail.com>
2023-04-03 16:49:51 +0900

    Initial Commit

---
+++ AppleMusicBottomSheet/AppleMusicBottomSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicBottomSheetApp.swift
+//  AppleMusicBottomSheet
+//
+//  Created by James on 2023/04/03.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicBottomSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
